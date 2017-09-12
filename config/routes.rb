0733776fb65a16c74f6421cd06be99b0b1ef059d
Melvin Brown <melvinbrown1993@gmail.com>
2017-09-12 13:02:12 +0000

    Added form fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to:'pages#home'  # Controller#action
   get 'about', to: 'pages#about'
-  resources :contacts
+  resources :contacts #get 'contacts/new', to 'contacts#new'
 end
