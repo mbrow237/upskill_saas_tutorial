06a839d4a6e5b295cf45041d6c48432e87c4b731
Melvin Brown <melvinbrown1993@gmail.com>
2017-09-20 15:45:16 +0000

    Created contacts table, checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to:'pages#home'  # Controller#action
   get 'about', to: 'pages#about'
-  resources :contacts #get 'contacts/new', to 'contacts#new'
+  resources :contacts #get 'contacts/new', to 'contacts#new' #Also resources add routes to new, edit, show, update, and destroy pages
 end
